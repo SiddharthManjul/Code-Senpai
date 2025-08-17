b270a4b046583bf508f505bef21c3e90d37f7aa1
SiddharthManjul <manjulsiddharth09@gmail.com>
2025-08-17 23:19:33 +0530

    Wallet Provider - Dynamic

---
+++ src/lib/providers.tsx
@@ -0,0 +1,76 @@
+'use client';
+ 
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { createConfig, WagmiProvider } from 'wagmi';
+import { http } from 'viem';
+import { sei, seiTestnet } from 'viem/chains';
+ 
+// Dynamic imports
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+import { DynamicContextProvider, DynamicWidget } from '@dynamic-labs/sdk-react-core';
+import { DynamicWagmiConnector } from '@dynamic-labs/wagmi-connector';
+import { EthereumWalletConnectors } from '@dynamic-labs/ethereum';
+ 
+// Import Sei Global Wallet for EIP-6963 discovery
+import '@sei-js/sei-global-wallet/eip6963';
+ 
+const queryClient = new QueryClient();
+ 
+const wagmiConfig = createConfig({
+  chains: [sei, seiTestnet],
+  transports: {
+    [sei.id]: http('https://evm-rpc.sei-apis.com'),
+    [seiTestnet.id]: http('https://evm-rpc-testnet.sei-apis.com')
+  }
+});
+ 
+export default function Providers({ children }: { children: React.ReactNode }) {
+  return (
+    <DynamicContextProvider
+      settings={{
+        environmentId: 'REPLACE-WITH-YOUR-ENVIRONMENT-ID', // Replace with your environmentId
+        walletConnectors: [EthereumWalletConnectors],
+        overrides: {
+          evmNetworks: (networks) => [
+            ...networks,
+            {
+              blockExplorerUrls: ['https://seitrace.com'],
+              chainId: 1329,
+              chainName: 'Sei Network',
+              iconUrls: ['https://app.dynamic.xyz/assets/networks/sei.svg'],
+              name: 'Sei',
+              nativeCurrency: {
+                decimals: 18,
+                name: 'Sei',
+                symbol: 'SEI'
+              },
+              networkId: 1329,
+              rpcUrls: ['https://evm-rpc.sei-apis.com'],
+              vanityName: 'Sei Mainnet'
+            },
+            {
+              blockExplorerUrls: ['https://seitrace.com/?chain=testnet'],
+              chainId: 1328,
+              chainName: 'Sei Testnet',
+              iconUrls: ['https://app.dynamic.xyz/assets/networks/sei.svg'],
+              name: 'Sei Testnet',
+              nativeCurrency: {
+                decimals: 18,
+                name: 'Sei',
+                symbol: 'SEI'
+              },
+              networkId: 1328,
+              rpcUrls: ['https://evm-rpc-testnet.sei-apis.com'],
+              vanityName: 'Sei Testnet'
+            }
+          ]
+        }
+      }}>
+      <WagmiProvider config={wagmiConfig}>
+        <QueryClientProvider client={queryClient}>
+          <DynamicWagmiConnector>{children}</DynamicWagmiConnector>
+        </QueryClientProvider>
+      </WagmiProvider>
+    </DynamicContextProvider>
+  );
+}
